299961905561d95e3cc3e32b2921b3d8de0a30ba
Ikroop Burmi <ikburmi@gmail.com>
2023-06-27 09:30:03 -0400

    Initial Commit

---
+++ DestiFinder/DestiFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  DestiFinderApp.swift
+//  DestiFinder
+//
+//  Created by Scholar on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct DestiFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
