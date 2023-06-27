dadf3e028849cf2ba9ff429ebabeee608a82aaba
Navya Varda <navya.varda@gmail.com>
2023-06-27 10:02:12 -0400

    pink heart!!!

--- DestiFinder/ContentView.swift
+++ DestiFinder/ContentView.swift
@@ -10,9 +10,9 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "heart.fill")
                 .imageScale(.large)
-                .foregroundColor(.accentColor)
+                .foregroundColor(.pink)
             Text("Hello, world!")
                 .foregroundColor(Color.pink)
             //pink is the best!!!
