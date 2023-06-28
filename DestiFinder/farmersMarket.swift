368715a29987a5f870dc7684f4700139be9c6f02
Kelly Kim <kellykim240@gmail.com>
2023-06-28 14:39:47 -0400

    hi

---
+++ DestiFinder/farmersMarket.swift
@@ -0,0 +1,20 @@
+//
+//  farmersMarket.swift
+//  DestiFinder
+//
+//  Created by Scholar on 6/28/23.
+//
+
+import SwiftUI
+
+struct farmersMarket: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct farmersMarket_Previews: PreviewProvider {
+    static var previews: some View {
+        farmersMarket()
+    }
+}
